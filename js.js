91b26398a8168c2a0f5b8a44ceb49d81ac5b858c
gangothri-818 <pilligangothri@gmail.com>
2025-04-08 21:39:17 +0530

    Update js.js

---
+++ js.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll('nav a[href^="#"]');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
